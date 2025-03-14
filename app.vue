4022a7bb8df325371be04bd40ff04b8b5fb09a7c
Arnaud Steiner <arnaudsteiner@orange.fr>
2025-03-14 10:55:31 +0100

    build: nuxt hot reload enabled and tailwind nuxt module replaces original tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-	<h1 class="text-3xl font-bold underline">
+	<h1 class="text-3xl font-bold underline bg-amber-300">
 		Hello world!
 	</h1>
 </template>
---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./components/**/*.{vue,js,ts}',
+		'./pages/**/*.vue',
+		'./nuxt.config.{js,ts}',
+	],
+	theme: {
+		extend: {
+			colors: {
+
+			},
+			borderColor: {
+
+			},
+		},
+	},
+	plugins: [],
+};
